b88730afd6dae0de33846bc2d001812026fd9207
mosa <info@example.com>
2009-12-15 18:02:56 +0000

    Licensing terms change
    git-svn-id: http://svn.projects.ez.no/mcalendar@20 b87a8fb1-9f58-42a2-a0a4-5798444208a8

--- trunk/classes/ezcalendarEvents.php
+++ trunk/classes/ezcalendarEvents.php
@@ -1,11 +1,8 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
- * Description of ezcoursesSubscription
+ * 
  *
  * @author mosa
  */
