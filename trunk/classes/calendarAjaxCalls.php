de1466cc0da06da882232b273e516e89ac343c56
mosa <info@example.com>
2009-12-15 17:52:24 +0000

    updateEvent remove
    git-svn-id: http://svn.projects.ez.no/mcalendar@8 b87a8fb1-9f58-42a2-a0a4-5798444208a8

--- trunk/classes/calendarAjaxCalls.php
+++ trunk/classes/calendarAjaxCalls.php
@@ -21,7 +21,7 @@ class calendarAjaxCalls {
                 $node->removeNodeFromTree(false);
             else:
                 return 'You are not allowed to remove this node';
-        endif;
+            endif;
         }
         return 'Node removal succesfull';
     }
@@ -33,21 +33,34 @@ class calendarAjaxCalls {
 *
 ****************************************************************/
 
-public static function updateEventAjax($args) {
+    public static function updateEventAjax($args) {
         $objectId=$args[0];
-        $fromTime=(int)$args[1];
-        $toTime=(int)$args[2];
+        $arguments=json_decode($_POST['postdata'],true);
         $event=eZContentObject::fetch($objectId);
-
         if($event->canEdit()) {
+            $xmlText='<?xml version="1.0" encoding="utf-8"?>
+                <section xmlns:image="http://ez.no/namespaces/ezpublish3/image/"
+                         xmlns:xhtml="http://ez.no/namespaces/ezpublish3/xhtml/"
+                         xmlns:custom="http://ez.no/namespaces/ezpublish3/custom/">
+                        <paragraph>'.$arguments['text'].'</paragraph></section>';
+
+            $mod_attributes=array('to_time'=>array('data_int',(int)$args[2]),
+                'from_time'=>array('data_int',(int)$args[1]),
+                'short_title'=>array('data_text',$arguments['short_title']),
+                'text'=>array('data_text',$xmlText));
+
             $datamap=$event->attribute('data_map');
-            $toTimeAttribute=$datamap['to_time'];
-            $fromTimeAttribute=$datamap['from_time'];
-            $toTimeAttribute->setAttribute('data_int',$toTime);
-            $toTimeAttribute->sync();
-            $fromTimeAttribute->setAttribute('data_int',$fromTime);
-            $fromTimeAttribute->sync();}else {
-            return 'You are not allowed to edit this event';}
+            foreach($mod_attributes as $key=>$content) {
+                $current_attribute=$datamap[$key];
+                $current_attribute->setContent($content[1]);
+                $current_attribute->setAttribute($content[0],$content[1]);
+                $current_attribute->sync();
+            }
+
+        }
+        else {
+            return 'You are not allowed to edit this event';
+        }
         return 'Edit success';
 
     }
@@ -65,18 +78,18 @@ public static function updateEventAjax($args) {
         $parentNodeId=$args[0];
         $fromTime=$args[1];
         $toTime=$args[2];
-        $simpleText=$args[3];
-        $title=$args[4];
+        $arguments=json_decode($_POST['postdata'],true);
+
         $xmlText='<?xml version="1.0" encoding="utf-8"?>
                 <section xmlns:image="http://ez.no/namespaces/ezpublish3/image/"
                          xmlns:xhtml="http://ez.no/namespaces/ezpublish3/xhtml/"
                          xmlns:custom="http://ez.no/namespaces/ezpublish3/custom/">
-                        <paragraph>'.$simpleText.'</paragraph></section>';
+                        <paragraph>'.$arguments['text'].'</paragraph></section>';
         $params = array();
         $params['creator_id'] = $user->id();
         $params['parent_node_id'] = (int)$parentNodeId;
         $params['class_identifier'] = 'event';
-        $params['attributes']=array('short_title'=>$title,'text'=>$xmlText,
+        $params['attributes']=array('short_title'=>$arguments['short_title'],'text'=>$xmlText,
             'from_time'=>$fromTime,'to_time'=>$toTime,'frequency'=>'0','frequency_end'=>$toTime);
         $newObject = eZContentFunctions::createAndPublishObject($params);
         $node = eZContentObjectTreeNode::fetch( $newObject->attribute( 'main_node_id' ) );
@@ -85,6 +98,7 @@ public static function updateEventAjax($args) {
         $data['object_id']=$newObject->ID;
         $data['node_id']=$newObject->attribute( 'main_node_id' );
         $data['current_language']=$newObject->currentLanguage();
+        $data['title']=$arguments['short_title'];
         return $data;
 
     }
@@ -99,25 +113,23 @@ public static function updateEventAjax($args) {
         $parentNodeId=$args[0];
         $fromTime=$args[1];
         $toTime=$args[2];
-        $simpleText=$args[3];
-        $title=$args[4];
-        $ajaxid=(int)$args[5];
+        $arguments=json_decode($_POST['postdata'],true);
         $xmlText='<?xml version="1.0" encoding="utf-8"?>
                 <section xmlns:image="http://ez.no/namespaces/ezpublish3/image/"
                          xmlns:xhtml="http://ez.no/namespaces/ezpublish3/xhtml/"
                          xmlns:custom="http://ez.no/namespaces/ezpublish3/custom/">
-                        <paragraph>'.$simpleText.'</paragraph></section>';
+                        <paragraph>'.$arguments['text'].'</paragraph></section>';
         $params = array();
         $params['creator_id'] = $user->id();
         $params['parent_node_id'] = (int)$parentNodeId;
         $params['class_identifier'] = 'event';
-        $params['attributes']=array('short_title'=>$title,'text'=>$xmlText,
+        $params['attributes']=array('short_title'=>$arguments['short_title'],'text'=>$xmlText,
             'from_time'=>$fromTime,'to_time'=>$toTime,'frequency'=>'0','frequency_end'=>$toTime);
         $newObject = eZContentFunctions::createAndPublishObject($params);
         $node = eZContentObjectTreeNode::fetch( $newObject->attribute( 'main_node_id' ) );
 
-        $data=array('id'=>$ajaxid,
-            'start'=>$fromTime*1000,'end'=>$toTime*1000,'title'=>$title,
+        $data=array('id'=>$arguments['id'],
+            'start'=>$fromTime*1000,'end'=>$toTime*1000,'title'=>$arguments['short_title'],
             'nodeId'=>(int)($newObject->attribute( 'main_node_id' )),'parentNodeId'=>(int)$parentNodeId,
             'urlAlias'=>$node->urlAlias(),'currentLanguage'=>$newObject->currentLanguage(),
             'objectId'=>$newObject->ID,'frequency'=>0,'isMain'=>true);
